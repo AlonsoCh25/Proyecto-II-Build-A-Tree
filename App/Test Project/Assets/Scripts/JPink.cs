c62148b3dd0d90db291ab79f02faeb44073a8266
mmtx2000 <mtx202012@gmail.com>
2020-11-25 00:10:29 -0600

    Anim/Saltos
    Se realizaron las 5 animaciones de los cuatro personajes.
    Se programó las función de salto normal en cada personaje

---
+++ App/Test Project/Assets/Scripts/JPink.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JPink : MonoBehaviour
+{
+    private Animator animator;
+
+    public float FuezadeSalto;
+
+    private Rigidbody2D _rigidbody2D;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            animator.SetBool("JumpPink", true);
+            _rigidbody2D.AddForce(new Vector2(0, FuezadeSalto));
+        }
+    }
+}
