0820de4b806d60866ca50be5572074e1b98c0db1
Olman Rodriguez <irodriguezcr1@gmail.com>
2020-12-09 00:45:55 -0600

    Arbol AVL

--- Servidor/src/Main.java
+++ Servidor/src/Main.java
@@ -1,21 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-	//Server server = new Server();
-	//Thread s = new Thread(server);
-	//s.start();
-		BTS bts = new BTS();
-        System.out.println(bts.isEmpty());
-		bts.insert(2);
-		bts.insert(3);
-		bts.insert(1);
-		bts.insert(4);
-        System.out.println(bts.isEmpty());
-        System.out.println(bts.FindMin().getData());
-        System.out.println(bts.FindMax().getData());
-        bts.Delete(4);
-        bts.Delete(1);
-        bts.Delete(3);
-        bts.Delete(2);
-        System.out.println(bts.isEmpty());
+	Server server = new Server();
+	Thread s = new Thread(server);
+	s.start();
     }
 }
---
+++ Servidor/src/NodeAVL.java
@@ -0,0 +1,38 @@
+public class NodeAVL {
+    Object data;
+    NodeAVL right;
+    NodeAVL left;
+    int height;
+
+    public NodeAVL (Object data){
+        this.data = data;
+        this.left = null;
+        this.right = null;
+        this.height = 0;
+
+    }
+    public void setData(Object data) {
+        this.data = data;
+    }
+    public Object getData() {
+        return data;
+    }
+    public void setLeft(NodeAVL left) {
+        this.left = left;
+    }
+    public void setRight(NodeAVL right) {
+        this.right = right;
+    }
+    public NodeAVL getRight() {
+        return right;
+    }
+    public NodeAVL getLeft() {
+        return left;
+    }
+    public void setHeight(int height) {
+        this.height = height;
+    }
+    public int getHeight() {
+        return height;
+    }
+}
